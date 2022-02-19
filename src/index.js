1aee887790d619576fa12f120e66eaa1e61d26cd
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-19 18:33:01 +0100

    added startServer function in /server

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ require("dotenv").config();
 const debug = require("debug")("robots:root");
 const chalk = require("chalk");
 const connectToMongoDB = require("./db");
+const startServer = require("./server");
 
 const mongoDBconnectionString = process.env.MONGO_DB_ATLAS_STRING;
+const port = process.env.PORT || 3000;
 
 (async () => {
   try {
     await connectToMongoDB(mongoDBconnectionString);
+    await startServer(port);
   } catch (error) {
     debug(chalk.red(`Error: `, error.message));
   }
