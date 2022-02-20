17e00a161330bb3da28a3491fb06552db18157a4
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-20 16:38:36 +0100

    added getRobot enpoint in router

--- src/server/routers/robotsRouter.js
+++ src/server/routers/robotsRouter.js
@@ -4,5 +4,6 @@ const { getAllRobots, getRobot } = require("../controllers/robotsControllers");
 const router = express.Router();
 
 router.get("/", getAllRobots);
+router.use("/:id", getRobot);
 
 module.exports = router;
