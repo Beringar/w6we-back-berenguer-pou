57d40327dbdb099f708826be97c6399beb22e439
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-21 20:31:29 +0100

    added usersControllers

---
+++ src/server/controllers/usersControllers.js
@@ -0,0 +1,21 @@
+const User = require("../../db/models/User");
+
+const userLogin = async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (user) {
+      res.json(user);
+      return;
+    }
+    const error = new Error("User not found!");
+    error.code = 404;
+    next(error);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  userLogin,
+};
